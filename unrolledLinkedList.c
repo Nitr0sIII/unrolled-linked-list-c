1fd7a961756503b83b9845fb700fdc9bb64209a4
Nitros <estebanchecot@gmail.com>
2026-03-07 20:45:22 +0100

    Optimisation of Insert function

--- unrolledLinkedList.c
+++ unrolledLinkedList.c
@@ -19,12 +19,17 @@ void printAllPage(Page *first);
 
 int main(int argc, char const *argv[]) {
   Page *first = constructorInit();
-  __int8_t a = 0;
-
-  // printf("\n\n%u\n\n", a | (1ULL << 8));
 
   insert(first, 5);
   insert(first, 85);
+  insert(first, 789);
+  insert(first, 45);
+  insert(first, 58);
+  insert(first, 22);
+  insert(first, 58);
+  insert(first, 2201);
+  insert(first, 5855);
+  insert(first, 2278);
 
   printAllPage(first);
   return 0;
@@ -45,20 +50,22 @@ Page *constructorInit() {
 }
 
 void insert(Page *firstPage, int element) {
-  if (firstPage == NULL)
+  if (firstPage == NULL) {
     return;
+  }
 
   Page *current = firstPage;
 
-  while (current != NULL) {
+  while (firstPage != NULL) {
     for (int i = 0; i < NB_ELEM; i++) {
       if (!(current->bitmap & (1ULL << i))) {
         current->element[i] = element;
         current->bitmap |= (1ULL << i);
-
-        // printf("Bitmap : %u", current->bitmap);
         return;
       }
+    }
+    if (current->next == NULL) {
+      current->next = constructorInit();
     }
     current = current->next;
   }
