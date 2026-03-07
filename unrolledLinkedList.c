70402da891edf2cfad82d71092ac9818d8d6fc39
Nitros <estebanchecot@gmail.com>
2026-03-07 20:56:55 +0100

    Free function added, best print function and insert function modify to be used with a **ptr, that allows to init de first page if it is NULL

--- unrolledLinkedList.c
+++ unrolledLinkedList.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,29 +8,29 @@ typedef struct Page Page;
 
 struct Page {
   int element[NB_ELEM];
-  __int8_t bitmap;
+  uint8_t bitmap;
   Page *next;
 };
 
 Page *constructorInit();
-void insert(Page *firstPage, int element);
+void insert(Page **firstPage, int element);
 void printAllPage(Page *first);
 
 //////////////////////////////////////////////////////////////////////
 
 int main(int argc, char const *argv[]) {
-  Page *first = constructorInit();
+  Page *first = NULL;
 
-  insert(first, 5);
-  insert(first, 85);
-  insert(first, 789);
-  insert(first, 45);
-  insert(first, 58);
-  insert(first, 22);
-  insert(first, 58);
-  insert(first, 2201);
-  insert(first, 5855);
-  insert(first, 2278);
+  insert(&first, 5);
+  insert(&first, 85);
+  insert(&first, 789);
+  insert(&first, 45);
+  insert(&first, 58);
+  insert(&first, 22);
+  insert(&first, 58);
+  insert(&first, 2201);
+  insert(&first, 5855);
+  insert(&first, 2278);
 
   printAllPage(first);
   return 0;
@@ -49,14 +50,14 @@ Page *constructorInit() {
   return tmpPage;
 }
 
-void insert(Page *firstPage, int element) {
-  if (firstPage == NULL) {
-    return;
+void insert(Page **firstPage, int element) {
+  if (*firstPage == NULL) {
+    *firstPage = constructorInit();
   }
 
-  Page *current = firstPage;
+  Page *current = *firstPage;
 
-  while (firstPage != NULL) {
+  while (current != NULL) {
     for (int i = 0; i < NB_ELEM; i++) {
       if (!(current->bitmap & (1ULL << i))) {
         current->element[i] = element;
@@ -75,12 +76,25 @@ void printAllPage(Page *first) {
   Page *current = first;
   int currentPageIndex = 0;
   while (current != NULL) {
-    printf("Page %d\n\n", currentPageIndex);
+    printf("Page %d\n", currentPageIndex);
     for (int i = 0; i < NB_ELEM; i++) {
-      printf("Elem %d : %d\n", i, current->element[i]);
+      if (current->bitmap & (1 << i)) {
+        printf("Elem %d : %d\n", i, current->element[i]);
+      } else {
+        printf("Elem %d : (vide)\n", i);
+      }
     }
-    printf("\n\n");
+    printf("\n");
     current = current->next;
     currentPageIndex++;
+  }
+}
+
+void freeAllPages(Page *first) {
+  Page *current = first;
+  while (current != NULL) {
+    Page *next = current->next;
+    free(current);
+    current = next;
   }
 }
